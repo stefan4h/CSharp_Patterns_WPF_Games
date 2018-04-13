fa8bd45ddfa6607300aaf9f5264af8e08e0076b8
Stefan Humpelstetter <s.humpelstetter@htlkrems.at>
2018-04-13 09:09:46 +0200

    AdapterPattern PersonToCustomer

---
+++ Adapter_Persom-To-Customer/Adapter_Persom-To-Customer/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Persom_To_Customer
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            this.Name = name;  
+        }
+    }
+}
---
+++ Adapter_Persom-To-Customer/Adapter_Persom-To-Customer/PersonToCustomer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Persom_To_Customer
+{
+    class PersonToCustomer : ICustomer
+    {
+        private string firstName;
+        private string lastName;
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        public PersonToCustomer(Person person)
+        {
+            firstName = person.Name.Split(' ')[0];
+            lastName = person.Name.Split(' ')[1];
+        }
+    }
+}
